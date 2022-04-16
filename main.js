01d9672cc764962a591745515a9462fc50663528
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-04-15 20:22:08 -0500

    async practice

---
+++ main.js
@@ -0,0 +1,11 @@
+function houseOne(){
+    console.log('Paper delivered to house 1')
+}
+function houseTwo(){
+    console.log('Paper delivered to house 2')
+}
+function houseThree(){
+    console.log('Paper delivered to house 3')
+}
+houseOne()
+houseTwo(houseThree)
